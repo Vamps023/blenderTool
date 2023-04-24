3edbba0647eb6ad2c002c2b87ed8c29179b51d08
Swapnil Nare <79307556+Vamps023@users.noreply.github.com>
2023-04-25 01:08:15 +0530

    fix():panel

--- gather_and_save_latest_version.py
+++ gather_and_save_latest_version.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Gather and Save Latest Version",
     "description": "Addon to gather latest file from specified path and save new version to same path",
-    "author": "Vamps (Swapnil Nare)",
-    "version": (1, 0),
+    "author": "Vamps(Swapnil Nare)",
+    "version": (1, 1),
     "blender": (3, 5, 0),
     "location": "View3D > Tool",
     "category": "Object"
@@ -12,137 +12,6 @@ import bpy
 import os
 
 class OBJECT_OT_gather_latest_version_operator(bpy.types.Operator):
-    """Operator to gather the latest version of the specified file"""
-    bl_idname = "object.gather_latest_version_operator"
-    bl_label = "Gather Latest Version"
-    
-    file_path: bpy.props.StringProperty(name="File Path")
-    file_prefix: bpy.props.StringProperty(name="File Prefix")
-    
-    def execute(self, context):
-        try:
-            files = os.listdir(self.file_path)
-            latest_version = 0
-            latest_file_path = ""
-            for f in files:
-                if f.endswith(".blend") and f.startswith(self.file_prefix):
-                    try:
-                        version = int(f[-7:-6])
-                        if version > latest_version:
-                            latest_version = version
-                            latest_file_path = os.path.join(self.file_path, f)
-                    except ValueError:
-                        pass
-            
-            if latest_file_path:
-                print("Opening file:", latest_file_path)
-                bpy.ops.wm.open_mainfile(filepath=latest_file_path)
-            else:
-                print("No valid file found.")
-        
-        except FileNotFoundError:
-            print("File path does not exist:", self.file_path)
-        
-        return {'FINISHED'}
-
-    """Gather latest version of the selected file"""
-    bl_idname = "object.gather_latest_version"
-    bl_label = "Gather Latest Version"
-    
-    prefix: bpy.props.StringProperty(default="", name="File Prefix")
-
-    def execute(self, context):
-        # Get the directory path
-        dir_path = os.path.dirname(bpy.data.filepath)
-        
-        # Get a list of files in the directory that start with the prefix
-        files = [f for f in os.listdir(dir_path) if f.startswith(self.prefix) and f.endswith(".blend")]
-
-        # Get the latest version number
-        latest_version = 0
-        for f in files:
-            try:
-                version = int(f.split("_v")[-1].split(".")[0])
-                if version > latest_version:
-                    latest_version = version
-            except ValueError:
-                pass
-        
-        # Open the latest version file
-        if latest_version > 0:
-            latest_file_name = [f for f in files if f.endswith("_v" + str(latest_version).zfill(3) + ".blend")][0]
-            latest_file_path = os.path.join(dir_path, latest_file_name)
-            bpy.ops.wm.open_mainfile(filepath=latest_file_path)
-            self.report({'INFO'}, "Opened file: " + latest_file_name)
-        else:
-            self.report({'ERROR'}, "No matching files found")
-        
-        return {'FINISHED'}
-    """Operator to gather the latest version of the specified file"""
-    bl_idname = "object.gather_latest_version_operator"
-    bl_label = "Gather Latest Version"
-    
-    file_path: bpy.props.StringProperty(name="File Path")
-    file_prefix: bpy.props.StringProperty(name="File Prefix")
-    
-    def execute(self, context):
-        try:
-            files = os.listdir(self.file_path)
-            latest_version = 0
-            latest_file_path = ""
-            for f in files:
-                if f.endswith(".blend") and f.startswith(self.file_prefix):
-                    try:
-                        version = int(f[-7:-6])
-                        if version > latest_version:
-                            latest_version = version
-                            latest_file_path = os.path.join(self.file_path, f)
-                    except ValueError:
-                        pass
-            
-            if latest_file_path:
-                print("Opening file:", latest_file_path)
-                bpy.ops.wm.open_mainfile(filepath=latest_file_path)
-            else:
-                print("No valid file found.")
-        
-        except FileNotFoundError:
-            print("File path does not exist:", self.file_path)
-        
-        return {'FINISHED'}
-
-    """Operator to gather the latest version of the specified file"""
-    bl_idname = "object.gather_latest_version_operator"
-    bl_label = "Gather Latest Version"
-    
-    file_path: bpy.props.StringProperty(name="File Path")
-    
-    def execute(self, context):
-        try:
-            files = os.listdir(self.file_path)
-            latest_version = 0
-            latest_file_path = ""
-            for f in files:
-                if f.endswith(".blend") and f.startswith("_"):
-                    try:
-                        version = int(f[-7:-6])
-                        if version > latest_version:
-                            latest_version = version
-                            latest_file_path = os.path.join(self.file_path, f)
-                    except ValueError:
-                        pass
-            
-            if latest_file_path:
-                print("Opening file:", latest_file_path)
-                bpy.ops.wm.open_mainfile(filepath=latest_file_path)
-            else:
-                print("No valid file found.")
-        
-        except FileNotFoundError:
-            print("File path does not exist:", self.file_path)
-        
-        return {'FINISHED'}
-
     """Operator to gather latest version of specified file"""
     bl_idname = "object.gather_latest_version_operator"
     bl_label = "Gather Latest Version"
@@ -212,60 +81,7 @@ class OBJECT_OT_save_new_version_operator(bpy.types.Operator):
 
         return {'FINISHED'}
 
-class OBJECT_PT_gather_latest_version_panel(bpy.types.Panel):
-    """Panel for Gather Latest Version addon"""
-    bl_idname = "OBJECT_PT_gather_latest_version_panel"
-    bl_label = "Gather Latest Version"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        
-        layout.label(text="File Path:")
-        layout.prop(scene.gather_latest_version_props, "file_path", text="")
-        
-        layout.label(text="File Prefix:")
-        layout.prop(scene.gather_latest_version_props, "file_prefix", text="")
-        
-        layout.separator()
-        layout.operator("object.gather_latest_version_operator", text="Gather Latest Version")
-
-    """Panel for Gather Latest Version addon"""
-    bl_idname = "OBJECT_PT_gather_latest_version_panel"
-    bl_label = "Gather Latest Version"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-
-        row = layout.row()
-        row.operator("object.gather_latest_version", text="Gather Latest Version")
-
-        row = layout.row()
-        row.prop(context.scene, "prefix")
-    """Panel for Gather Latest Version addon"""
-    bl_idname = "OBJECT_PT_gather_latest_version_panel"
-    bl_label = "Gather Latest Version"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        
-        layout.label(text="File Path:")
-        layout.prop(scene.gather_latest_version_props, "file_path", text="")
-        
-        layout.label(text="File Prefix:")
-        layout.prop(scene.gather_latest_version_props, "file_prefix", text="")
-        
-        layout.separator()
-        layout.operator("object.gather_latest_version_operator", text="Gather Latest Version")
-
+class OBJECT_PT_gather_and_save_latest_version_panel(bpy.types.Panel):
     """Panel for Gather and Save Latest Version addon"""
     bl_idname = "OBJECT_PT_gather_and_save_latest_version_panel"
     bl_label = "Gather and Save Latest Version"
